e6621f8f1bf1ee8d2b2bd850e868f507afa5b310
Arjun more <amore@chirpn.com>
2023-03-30 00:22:34 +0530

    ui changes

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const app = express();
 const port = 8080;
 
 // use express router
-app.use('/', routes);
 app.use(express.urlencoded());
 app.use(express.static('assets'));
+
+
+app.use('/', routes);
+
 
 
 app.set('view engine', 'ejs')
@@ -17,6 +20,5 @@ app.listen(port, function (err) {
     if (err) {
         console.log(`Error in running the server: ${err}`);
     }
-
     console.log(`Server is running on port: ${port}`);
 });
